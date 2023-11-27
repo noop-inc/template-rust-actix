217bc4ebe4985ed73fb79a02de445bfeefdf9619
Joe Kendall <joe.kendall@noop.dev>
2023-11-27 16:28:44 -0500

    modifying example to follow rust demo that uses postgres and environment variables

---
+++ sql/schemas.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+	id  BIGSERIAL PRIMARY KEY,
+	email       VARCHAR(200) NOT NULL,
+	first_name  VARCHAR(200) NOT NULL,
+	last_name   VARCHAR(200) NOT NULL,
+	username    VARCHAR(50) UNIQUE NOT NULL,
+	UNIQUE (username)
+);
